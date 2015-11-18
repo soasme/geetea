e5497a6ca90b8c3243bd5f590f6edadb2523965e
soasme <soasme@gmail.com>
2015-11-18 16:27:24 +0800

    Remove useless codes.

--- www/js/libs/oauth/utils.js
+++ www/js/libs/oauth/utils.js
@@ -19,56 +19,6 @@ angular.module("libs.oauth.utils", [])
             },
 
             /*
-             * Sign an Oauth 1.0 request
-             *
-             * @param    string method
-             * @param    string endPoint
-             * @param    object headerParameters
-             * @param    object bodyParameters
-             * @param    string secretKey
-             * @param    string tokenSecret (optional)
-             * @return   object
-             */
-            createSignature: function(method, endPoint, headerParameters, bodyParameters, secretKey, tokenSecret) {
-                if(typeof jsSHA !== "undefined") {
-                    var headerAndBodyParameters = angular.copy(headerParameters);
-                    var bodyParameterKeys = Object.keys(bodyParameters);
-                    for(var i = 0; i < bodyParameterKeys.length; i++) {
-                        headerAndBodyParameters[bodyParameterKeys[i]] = encodeURIComponent(bodyParameters[bodyParameterKeys[i]]);
-                    }
-                    var signatureBaseString = method + "&" + encodeURIComponent(endPoint) + "&";
-                    var headerAndBodyParameterKeys = (Object.keys(headerAndBodyParameters)).sort();
-                    for(i = 0; i < headerAndBodyParameterKeys.length; i++) {
-                        if(i == headerAndBodyParameterKeys.length - 1) {
-                            signatureBaseString += encodeURIComponent(headerAndBodyParameterKeys[i] + "=" + headerAndBodyParameters[headerAndBodyParameterKeys[i]]);
-                        } else {
-                            signatureBaseString += encodeURIComponent(headerAndBodyParameterKeys[i] + "=" + headerAndBodyParameters[headerAndBodyParameterKeys[i]] + "&");
-                        }
-                    }
-                    var oauthSignatureObject = new jsSHA(signatureBaseString, "TEXT");
-
-                    var encodedTokenSecret = '';
-                    if (tokenSecret) {
-                        encodedTokenSecret = encodeURIComponent(tokenSecret);
-                    }
-
-                    headerParameters.oauth_signature = encodeURIComponent(oauthSignatureObject.getHMAC(encodeURIComponent(secretKey) + "&" + encodedTokenSecret, "TEXT", "SHA-1", "B64"));
-                    var headerParameterKeys = Object.keys(headerParameters);
-                    var authorizationHeader = 'OAuth ';
-                    for(i = 0; i < headerParameterKeys.length; i++) {
-                        if(i == headerParameterKeys.length - 1) {
-                            authorizationHeader += headerParameterKeys[i] + '="' + headerParameters[headerParameterKeys[i]] + '"';
-                        } else {
-                            authorizationHeader += headerParameterKeys[i] + '="' + headerParameters[headerParameterKeys[i]] + '",';
-                        }
-                    }
-                    return { signature_base_string: signatureBaseString, authorization_header: authorizationHeader, signature: headerParameters.oauth_signature };
-                } else {
-                    return "Missing jsSHA JavaScript library";
-                }
-            },
-
-            /*
             * Create Random String Nonce
             *
             * @param    integer length
@@ -83,6 +33,9 @@ angular.module("libs.oauth.utils", [])
                 return text;
             },
 
+            /*
+             * Generate Url Parameters Object From String.
+             */
             generateUrlParameters: function (parameters) {
                 var sortedKeys = Object.keys(parameters);
                 sortedKeys.sort();
@@ -98,6 +51,9 @@ angular.module("libs.oauth.utils", [])
                 return params;
             },
 
+            /*
+             * Parse Response Parameters String From Object.
+             */
             parseResponseParameters: function (response) {
                 if (response.split) {
                     var parameters = response.split("&");
@@ -110,20 +66,7 @@ angular.module("libs.oauth.utils", [])
                 else {
                     return {};
                 }
-            },
-
-            generateOauthParametersInstance: function(consumerKey) {
-                var nonceObj = new jsSHA(Math.round((new Date()).getTime() / 1000.0), "TEXT");
-                var oauthObject = {
-                    oauth_consumer_key: consumerKey,
-                    oauth_nonce: nonceObj.getHash("SHA-1", "HEX"),
-                    oauth_signature_method: "HMAC-SHA1",
-                    oauth_timestamp: Math.round((new Date()).getTime() / 1000.0),
-                    oauth_version: "1.0"
-                };
-                return oauthObject;
             }
-
         };
 
     }]);
