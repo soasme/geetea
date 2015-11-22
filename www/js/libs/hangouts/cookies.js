2f5d47c8fffe90e457768304bf2e1ddc6866b0f5
soasme <soasme@gmail.com>
2015-11-22 18:28:00 +0800

    Call Google OAuth Login and MergeSesion APIs.

--- www/js/libs/hangouts/cookies.js
+++ www/js/libs/hangouts/cookies.js
@@ -1,6 +1,34 @@
 angular
   .module('libs.hangouts.cookies', ['ngCookies'])
   .factory('hangoutsCookies', ['$http', '$cookies', '$q', function($http, $cookies, $q) {
+
+    var _getHeaders = function(tokenType, accessToken) {
+      return {
+        Authorization: tokenType + ' ' + accessToken
+      };
+    };
+
+    var _OAuthLogin = function(headers) {
+      return $q(function(resolve, reject) {
+        var uberAuthURL = 'https://accounts.google.com/accounts/OAuthLogin?source=geetea&issueuberauth=1';
+        $http.get(uberAuthURL, {headers: headers}).then(function(resp) {
+          resolve(resp.data);
+        }, function(resp) {
+          reject(resp.data);
+        });
+      });
+    };
+
+    var _mergeSession = function(ubertext, headers) {
+      return $q(function(resolve, reject) {
+        var mergeSessionURL = 'https://accounts.google.com/MergeSession?service=mail&continue=http://www.google.com&uberauth=' + ubertext;
+        $http.get(mergeSessionURL, {headers: headers}).then(function(resp) {
+          resolve($cookies.getAll());
+        }, function(resp) {
+          reject(resp.data);
+        });
+      });
+    };
     return {
     };
   }]);
