4a8337bf24523d7e01563b27c0a8d97da64b8641
soasme <soasme@gmail.com>
2015-11-22 18:28:33 +0800

    Setup cookies module for test.

--- www/js/libs/hangouts/cookies_test.js
+++ www/js/libs/hangouts/cookies_test.js
@@ -1,5 +1,12 @@
 'use strict';
 
 describe('libs.hangouts.cookies module', function() {
+  var hangoutsCookies, $httpBackend, $cookies, $rootScope;
   beforeEach(module('libs.hangouts.cookies'));
+  beforeEach(inject(function($injector, _hangoutsCookies_, _$cookies_, _$rootScope_) {
+    hangoutsCookies = _hangoutsCookies_;
+    $rootScope = _$rootScope_;
+    $cookies = _$cookies_;
+    $httpBackend = $injector.get('$httpBackend');
+  }));
 });
