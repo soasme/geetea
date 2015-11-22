8ca0280bd039a38cdcbfd5969f7cd8d1e5efe345
soasme <soasme@gmail.com>
2015-11-22 21:32:54 +0800

    Get $httpBackend directly, without getting through $injector

--- www/js/libs/hangouts/cookies_test.js
+++ www/js/libs/hangouts/cookies_test.js
@@ -3,11 +3,11 @@
 describe('libs.hangouts.cookies module', function() {
   var hangoutsCookies, $httpBackend, $cookies, $rootScope;
   beforeEach(module('libs.hangouts.cookies'));
-  beforeEach(inject(function($injector, _hangoutsCookies_, _$cookies_, _$rootScope_) {
+  beforeEach(inject(function(_$httpBackend_, _hangoutsCookies_, _$cookies_, _$rootScope_) {
     hangoutsCookies = _hangoutsCookies_;
     $rootScope = _$rootScope_;
     $cookies = _$cookies_;
-    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend = _$httpBackend_;
   }));
 
 
