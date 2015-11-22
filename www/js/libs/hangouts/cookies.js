af1f8fd22d3f833aa7b314d852817094a5d31e24
soasme <soasme@gmail.com>
2015-11-22 18:23:30 +0800

    Init `libs.hangouts.cookies` module.

---
+++ www/js/libs/hangouts/cookies.js
@@ -0,0 +1,6 @@
+angular
+  .module('libs.hangouts.cookies', ['ngCookies'])
+  .factory('hangoutsCookies', ['$http', '$cookies', '$q', function($http, $cookies, $q) {
+    return {
+    };
+  }]);
---
+++ www/js/libs/hangouts/cookies_test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('libs.hangouts.cookies module', function() {
+  beforeEach(module('libs.hangouts.cookies'));
+});
