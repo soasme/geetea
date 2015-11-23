9899f6ac4676272c275b29695cba752ac8e8e41f
soasme <soasme@gmail.com>
2015-11-23 21:53:14 +0800

    Inject dependencies via $inject property.

--- www/js/services/authorization.js
+++ www/js/services/authorization.js
@@ -7,35 +7,33 @@
       'libs.hangouts.cookies',
       'services.authStorage',
     ])
-    .factory('authorization', [
-      '$q',
-      'googleOAuth',
-      'authStorage',
-      'hangoutsCookies',
-      authorization
-    ]);
+    .factory('authorization', authorization);
+
+  authorization.$inject = ['$q', 'googleOAuth', 'authStorage', 'hangoutsCookies'];
 
   function authorization( $q, googleOAuth, authStorage, hangoutsCookies) {
     return {
-      obtain: function(clientId, scope) {
-        return $q(function(resolve, reject) {
-          if (authStorage.has()) {
-            var cookies = authStorage.get();
-            resolve(cookies);
-          } else {
-            googleOAuth.auth(clientId, scope).then(function(token) {
-              hangoutsCookies.get(token.tokenType, token.accessToken).then(function(cookies) {
-                authStorage.set(cookies);
-                resolve(cookies);
-              }, function(reason) {
-                reject(reason);
-              });
+      obtain: obtain
+    };
+
+    function obtain(clientId, scope) {
+      return $q(function(resolve, reject) {
+        if (authStorage.has()) {
+          var cookies = authStorage.get();
+          resolve(cookies);
+        } else {
+          googleOAuth.auth(clientId, scope).then(function(token) {
+            hangoutsCookies.get(token.tokenType, token.accessToken).then(function(cookies) {
+              authStorage.set(cookies);
+              resolve(cookies);
             }, function(reason) {
               reject(reason);
             });
-          }
-        });
-      }
-    };
+          }, function(reason) {
+            reject(reason);
+          });
+        }
+      });
+    }
   }
 })();
