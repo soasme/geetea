404e4fab40ec129c6f29803cc5334b54118394d8
soasme <soasme@gmail.com>
2015-11-23 21:30:04 +0800

    Wrap authorization module into closure.

--- www/js/services/authorization.js
+++ www/js/services/authorization.js
@@ -1,42 +1,44 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular
-  .module('services.authorization', [
-    'libs.oauth.google',
-    'libs.hangouts.cookies',
-    'services.authStorage',
-  ])
-  .factory('authorization', [
-    '$q',
-    'googleOAuth',
-    'authStorage',
-    'hangoutsCookies',
-    function(
-      $q,
-      googleOAuth,
-      authStorage,
-      hangoutsCookies
-    ) {
-      return {
-        obtain: function(clientId, scope) {
-          return $q(function(resolve, reject) {
-            if (authStorage.has()) {
-              var cookies = authStorage.get();
-              resolve(cookies);
-            } else {
-              googleOAuth.auth(clientId, scope).then(function(token) {
-                hangoutsCookies.get(token.tokenType, token.accessToken).then(function(cookies) {
-                  authStorage.set(cookies);
-                  resolve(cookies);
+  angular
+    .module('services.authorization', [
+      'libs.oauth.google',
+      'libs.hangouts.cookies',
+      'services.authStorage',
+    ])
+    .factory('authorization', [
+      '$q',
+      'googleOAuth',
+      'authStorage',
+      'hangoutsCookies',
+      function(
+        $q,
+        googleOAuth,
+        authStorage,
+        hangoutsCookies
+      ) {
+        return {
+          obtain: function(clientId, scope) {
+            return $q(function(resolve, reject) {
+              if (authStorage.has()) {
+                var cookies = authStorage.get();
+                resolve(cookies);
+              } else {
+                googleOAuth.auth(clientId, scope).then(function(token) {
+                  hangoutsCookies.get(token.tokenType, token.accessToken).then(function(cookies) {
+                    authStorage.set(cookies);
+                    resolve(cookies);
+                  }, function(reason) {
+                    reject(reason);
+                  });
                 }, function(reason) {
                   reject(reason);
                 });
-              }, function(reason) {
-                reject(reason);
-              });
-            }
-          });
-        }
-      };
-    }
-  ]);
+              }
+            });
+          }
+        };
+      }
+    ]);
+})();
