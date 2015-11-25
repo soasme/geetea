752aab8ed6a5451b86c6979749e1377f7f716213
soasme <soasme@gmail.com>
2015-11-25 13:01:31 +0800

    Use google closure library to construct our project.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,22 @@
-angular.module('starter.controllers', [])
+goog.provide('starter.controllers.DashCtrl');
+goog.provide('starter.controllers.ChatsCtrl');
+goog.provide('starter.controllers.ChatDetailCtrl');
+goog.provide('starter.controllers.AccountCtrl');
 
-.controller('DashCtrl', function($scope) {})
+// TODO: controllers below should be moved to separated modules.
 
-.controller('ChatsCtrl', function($scope, Chats) {
+starter.controllers.DashCtrl = function($scope) {
+};
+
+/*
+ * Chats Controller.
+ * @param {!angular.$scope} $scope
+ * @param {!starter.services.Chats} Chats
+ * @constructor
+ * @export
+ * @ngInject
+ */
+starter.controllers.ChatsCtrl = function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -15,14 +29,30 @@ angular.module('starter.controllers', [])
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
-})
+};
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+/*
+ * Chat Detail Controller.
+ * @param {!angular.$scope} $scope
+ * @param {!angular.$scopeParams} $scopeParams
+ * @param {!starter.services.Chats} Chats
+ * @constructor
+ * @export
+ * @ngInject
+ */
+starter.controllers.ChatDetailCtrl = function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
-})
+};
 
-.controller('AccountCtrl', function($scope) {
+/*
+ * Account Controller.
+ * @param {!angular.$scope} $scope
+ * @constructor
+ * @export
+ * @ngInject
+ */
+starter.controllers.AccountCtrl = function($scope) {
   $scope.settings = {
     enableFriends: true
   };
-});
+};
--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,6 @@
-angular.module('starter.services', [])
+goog.provide('starter.services.Chats');
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
+var chats = [{
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
@@ -31,20 +27,24 @@ angular.module('starter.services', [])
     face: 'img/mike.png'
   }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
+
+starter.services.Chats = function() { };
+
+starter.services.Chats.prototype.all = function() {
+  return chats;
+};
+
+starter.services.Chats.prototype.remove = function(chat) {
+  chats.splice(chats.indexOf(chat), 1);
+};
+
+starter.services.Chats.prototype.get = function(chatId) {
+  for (var i = 0; i < chats.length; i++) {
+    if (chats[i].id === parseInt(chatId)) {
+      return chats[i];
     }
-  };
-});
+  }
+  return null;
+};
+
+angular.module('starter.services', []).service('Chats', starter.services.Chats);
