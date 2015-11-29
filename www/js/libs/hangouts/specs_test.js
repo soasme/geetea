c748f7f71517cf5c6675ca5c9d2a25fae0842653
soasme <soasme@gmail.com>
2015-11-29 23:35:57 +0800

    Test decode and encode action.

--- www/js/libs/hangouts/specs_test.js
+++ www/js/libs/hangouts/specs_test.js
@@ -15,6 +15,12 @@
         // see proto definition.
         expect(hangoutsSpecs.FocusDevice.FOCUS_DEVICE_DESKTOP).toEqual(20);
       });
+
+      it("should encode+decode data success", function() {
+        var message = new hangoutsSpecs.LinkData("link_target");
+        var data = message.encode().toArrayBuffer();
+        expect(hangoutsSpecs.LinkData.decode(data).link_target).toEqual("link_target");
+      });
     })
 
   });
