86c83f14aed74adb2e2074322efb23d8f97d74c4
soasme <soasme@gmail.com>
2015-11-30 09:01:51 +0800

    Replace hardcoded protofile path to constant.

--- www/js/libs/hangouts/specs.js
+++ www/js/libs/hangouts/specs.js
@@ -5,10 +5,11 @@
     .module('libs.hangouts')
     .factory('hangoutsSpecs', hangoutsSpecs);
 
-  function hangoutsSpecs() {
+  // Need libs.hangouts provide `HANGOUT_SPECS_PROTO` constant.
+  function hangoutsSpecs(HANGOUT_SPECS_PROTO) {
     var ProtoBuf = dcodeIO.ProtoBuf;
     var ByteBuffer = dcodeIO.ByteBuffer;
-    var builder = ProtoBuf.loadProtoFile('./base/www/js/libs/hangouts/specs.proto');
+    var builder = ProtoBuf.loadProtoFile(HANGOUT_SPECS_PROTO);
     var root = builder.build();
     return root;
   }
